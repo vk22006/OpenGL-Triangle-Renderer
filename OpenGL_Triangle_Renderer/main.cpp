94f3e753cb2fa7a28fcde5359990a17eafeff2ef
vk22006 <kishorev22006@gmail.com>
2026-02-16 21:19:12 +0530

    Made few changes before tagging

--- OpenGL_Triangle_Renderer/main.cpp
+++ OpenGL_Triangle_Renderer/main.cpp
@@ -21,11 +21,7 @@ const char* fragmentShaderSource = "#version 330 core\n"
 	"{\n"
 	"   FragColor = vec4(1.1f, 0.8f, 0.2f, 1.0f);\n"    // Change the color of triangle here
 	"}\n\0";
-
-std::string LoadShaderFiles(const std::string& filename) {   // TODO: Make function to load shader files
-
-	//return shaderStr
-} 
+ 
 
 static void processInput(GLFWwindow* window)
 {
